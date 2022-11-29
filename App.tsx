4f351d8a654e6c1f6b6d5dc39f3be6f121146706
Gabriel Oliveira <gabrieldanuzoliveira358@gmail.com>
2022-11-29 19:24:01 -0300

    Listando os estados

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { View } from 'react-native'
+import { FlatList, View, Text, StyleSheet } from 'react-native'
 import { api } from './src/services/api'
 
 interface Estado {
@@ -14,11 +14,68 @@ export default function App() {
 
   async function carregaEstados() {
     const response = await api.get('?orderBy=nome')
+    setEstados(response.data)
   }
 
   useEffect(() => {
     carregaEstados()
   }, [])
 
-  return <View></View>
+  return (
+    <View style={styles.container}>
+      <FlatList<Estado>
+        style={{ flex: 1 }}
+        showsVerticalScrollIndicator={false}
+        data={estados}
+        keyExtractor={estado => String(estado.id)}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.itemEstado}>
+              <View style={styles.avatarSigla}>
+                <Text style={styles.sigla}>{item.sigla}</Text>
+              </View>
+
+              <Text style={styles.estado}>{item.nome}</Text>
+            </View>
+          )
+        }}
+      ></FlatList>
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#EEEEEE',
+    padding: 20,
+    justifyContent: 'center'
+  },
+  itemEstado: {
+    paddingVertical: 10,
+    marginTop: 5,
+    backgroundColor: '#FFF',
+    flexDirection: 'row',
+    borderRadius: 10
+  },
+  avatarSigla: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#E76F51',
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 10
+  },
+  sigla: {
+    fontSize: 20,
+    color: 'white'
+  },
+  estado: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#264653',
+    marginLeft: 10,
+    alignSelf: 'center'
+  }
+})
